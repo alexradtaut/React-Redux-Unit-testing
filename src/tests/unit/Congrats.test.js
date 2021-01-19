cd843b4237265997eeb7f9a3ff514539c11f4df9
Alex Taut <alex.taut@SVROLP01894.ro.softvision.global>
2021-01-19 17:32:15 +0200

    Add to setupTests Enzyme config

--- src/tests/unit/Congrats.test.js
+++ src/tests/unit/Congrats.test.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import { findByTestAttr, checkProps } from '../testUtils';
-import checkPropTypes from 'check-prop-types';
 
 import Congrats from '../../components/Congrats';
 
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 const defaultProps = { success: false };
 
 const setup = (props = {}) => {
