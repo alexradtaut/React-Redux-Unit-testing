4a947b06ec842aaa49eb4d2947d5a6716974eada
Alex Taut <alex.taut@SVROLP01894.ro.softvision.global>
2021-01-20 16:02:35 +0200

    test secret word

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,10 @@
 import { getLetterMatchCount } from '../helpers';
+import axios from 'axios';
+
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
   GUESS_WORD: 'GUESS_WORD',
+  SET_SECRET_WORD: 'SET_SECRET_WORD',
 };
 
 export const guessWord = (guessedWord) => {
@@ -17,5 +20,15 @@ export const guessWord = (guessedWord) => {
         type: actionTypes.CORRECT_GUESS,
       });
     }
+  };
+};
+
+export const getSecretWord = () => {
+  return (dispatch) => {
+    return axios
+      .get('https://random-word-api.herokuapp.com/word?number=1')
+      .then((res) => {
+        dispatch({ type: actionTypes.SET_SECRET_WORD, payload: res.data });
+      });
   };
 };
