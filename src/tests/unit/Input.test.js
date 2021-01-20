5c6f12a777f2630a24955466daa46dc01ccc4ceb
Alex Taut <alex.taut@SVROLP01894.ro.softvision.global>
2021-01-20 11:35:02 +0200

    Input with not guessed word

--- src/tests/unit/Input.test.js
+++ src/tests/unit/Input.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 
 import { findByTestAttr } from '../testUtils';
 import Input from '../../components/Input';
-import storeFactory from '../testUtils';
+import { storeFactory } from '../testUtils';
 
 const setup = (initialState = {}) => {
   const store = storeFactory(initialState);
@@ -15,11 +15,25 @@ const setup = (initialState = {}) => {
 
 describe('render', () => {
   describe('word has not been guessed', () => {
-    test('renders component without error', () => {});
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: false };
+      wrapper = setup(initialState);
+    });
+    test('renders component without error', () => {
+      const component = findByTestAttr(wrapper, 'component-input');
+      expect(component.length).toBe(1);
+    });
 
-    test('renders input box', () => {});
+    test('renders input box', () => {
+      const inputBox = findByTestAttr(wrapper, 'input-box');
+      expect(inputBox.length).toBe(1);
+    });
 
-    test('renders submit button', () => {});
+    test('renders submit button', () => {
+      const submitButton = findByTestAttr(wrapper, 'submit-button');
+      expect(submitButton.length).toBe(1);
+    });
   });
 
   describe('word has been guessed', () => {
